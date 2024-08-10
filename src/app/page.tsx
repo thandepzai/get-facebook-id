92d1500d3f4e289b1e64b7440d48eeb73fe27084
Thandepzai <cuthan2k1@gmail.com>
2024-08-10 23:52:30 +0700

    add some

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
 export default async function Home() {
-  return (
-    <div>
-      <div style={{width: "400px", margin: "0 auto"}}>
-        <img
-          src="https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/452295054_2650775068433146_543023597824156080_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=POPg3jqjkUYQ7kNvgHM-rYX&_nc_ht=scontent.fhan18-1.fna&oh=00_AYASBn45DET0lRK-WAvlBPkZdgEiyZKU3X0tFZuYbw-W4w&oe=66BD5E8E"
-          alt=""
-          style={{width: "100%"}}
-        />
-      <span style={{paddingTop: "20px", fontSize: "20px", color:"pink"}}>Iuu emmm</span>
-      </div>
-    </div>
-  );
+  return <div>Hehe</div>;
 }
