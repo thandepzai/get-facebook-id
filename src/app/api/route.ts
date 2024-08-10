5768529ab41f4bc09a0bb4ce9499e91a3c121d1b
Thandepzai <cuthan2k1@gmail.com>
2024-08-11 00:23:05 +0700

    add some

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,9 +1,17 @@
 import { NextResponse } from "next/server";
 
+function fetchWithTimeout(url: string, options: any, timeout = 8000) {
+  return Promise.race([
+    fetch(url, options),
+    new Promise((_, reject) =>
+      setTimeout(() => reject(new Error("Request timeout")), timeout)
+    ),
+  ]);
+}
+
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const fburl = url.searchParams.get("url");
-
   try {
     const response = await fetch(
       `https://ffb.vn/api/tool/get-id-fb?idfb=${fburl}`
@@ -11,9 +19,10 @@ export async function GET(request: Request) {
 
     if (response.ok) {
       const htmlData = await response.json();
+      if (htmlData.id === "") throw new Error();
       return NextResponse.json({ id: htmlData.id }, { status: 200 });
     } else {
-      return NextResponse.json({ error: "No data found" }, { status: 404 });
+      throw new Error();
     }
   } catch (error) {
     console.error("Error:", error);
