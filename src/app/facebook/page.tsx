98edc8a79c985652c898c22bfecace02c77de259
thandepzai <91040602+thandepzai@users.noreply.github.com>
2024-08-28 14:23:46 +0700

    add some

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -16,13 +16,13 @@ export async function generateMetadata(
     openGraph: {
       title: "Thân đẹp zai",
       description: "Chưa biết ghi gì luôn",
-      url: "Mapstudy.com",
+        url: "Mapstudy.com",
       images:
-        "https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/455815989_473219259021016_1809018766819897025_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=xiXA_E5HlCEQ7kNvgFR_tR_&_nc_ht=scontent.fhan17-1.fna&oh=00_AYCNsTm0Br_y5dszUFHfvebs-6gk93WMJjIXGdPYG8a8Sg&oe=66D47FF9",
+        "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
     },
   };
 }
 
 export default function ShareView() {
-  return <div>abc</div>;
+  return <div></div>;
 }
