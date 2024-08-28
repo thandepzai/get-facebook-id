ccc624e942f6826bf33f85ed07d16931f7921ab2
thandepzai <91040602+thandepzai@users.noreply.github.com>
2024-08-28 15:13:36 +0700

    add some

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,34 +1,30 @@
-"use client";
-import { Metadata, ResolvingMetadata } from "next";
-import { useSearchParams } from "next/navigation";
+import type { Metadata, ResolvingMetadata } from "next";
 
-// type Props = {
-//   params: { id: string };
-//   searchParams: { [key: string]: string | string[] | undefined };
-// };
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   const title = searchParams.title
-//     ? String(searchParams.title)
-//     : "Default Title";
-//   const images = searchParams.images
-//     ? String(searchParams.images)
-//     : "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg";
-
-//   return {
-//     openGraph: {
-//       title: title,
-//       description: "Chưa biết ghi gì luôn",
-//       url: "https://mapstudy.edu.vn/",
-//       type: "website",
-//       phoneNumbers: "abc",
-//       images: images,
-//     },
-//   };
-// }
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const title = searchParams.title
+    ? String(searchParams.title)
+    : "Default Title";
+  const images = searchParams.images
+    ? String(searchParams.images)
+    : "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg";
+  return {
+    title: String(title),
+    openGraph: {
+      url: "https://mapstudy.edu.vn/",
+      images: images,
+      description: "Chúc mừng bạn tôi",
+      phoneNumbers: images,
+    },
+  };
+}
 
 export default function ShareView() {
   return <div>fsdfs</div>;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,12 @@
-"use client"
-import { useSearchParams } from "next/navigation";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
+
 }) {
-  const searchParams = useSearchParams();
-  const images = searchParams.get("images");
+
   return (
     <html lang="en">
-        <head>
-          <meta name="og:url" content="https://mapstudy.edu.vn/" />
-          <meta name="og:title" content="Thân đẹp zai" />
-          <meta name="og:description" content="Cái gì cơ" />
-          <meta name="og:type" content="website" />
-          <meta name="og:images" content={images ?? "abc"} />
-        </head>
       <body>{children}</body>
     </html>
   );
