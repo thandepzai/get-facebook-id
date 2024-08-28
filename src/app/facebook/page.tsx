1b45d44247da042d45ef52a9cfd7840c127738ce
thandepzai <91040602+thandepzai@users.noreply.github.com>
2024-08-28 14:45:59 +0700

    add some

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,33 +1,49 @@
+"use client";
 import { Metadata, ResolvingMetadata } from "next";
+import { useSearchParams } from "next/navigation";
 
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+// type Props = {
+//   params: { id: string };
+//   searchParams: { [key: string]: string | string[] | undefined };
+// };
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const title = searchParams.title
-    ? String(searchParams.title)
-    : "Default Title";
-  const images = searchParams.images
-    ? String(searchParams.images)
-    : "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg";
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const title = searchParams.title
+//     ? String(searchParams.title)
+//     : "Default Title";
+//   const images = searchParams.images
+//     ? String(searchParams.images)
+//     : "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg";
 
-  return {
-    openGraph: {
-      title: title,
-      description: "Chưa biết ghi gì luôn",
-      url: "https://mapstudy.edu.vn/",
-      type: "website",
-      phoneNumbers: "abc",
-      images: images,
-    },
-  };
-}
+//   return {
+//     openGraph: {
+//       title: title,
+//       description: "Chưa biết ghi gì luôn",
+//       url: "https://mapstudy.edu.vn/",
+//       type: "website",
+//       phoneNumbers: "abc",
+//       images: images,
+//     },
+//   };
+// }
 
 export default function ShareView() {
-  return <div></div>;
+  const searchParams = useSearchParams();
+  const images = searchParams.get("images");
+  console.log("🚀 ~ ShareView ~ images:", images);
+  return (
+    <div>
+      <head>
+        <meta name="og:url" content="https://mapstudy.edu.vn/" />
+        <meta name="og:title" content="Thân đẹp zai" />
+        <meta name="og:description" content="Cái gì cơ" />
+        <meta name="og:type" content="website" />
+        <meta name="og:images" content={images ?? "abc"} />
+      </head>
+      <img src="images" alt="" />
+    </div>
+  );
 }
